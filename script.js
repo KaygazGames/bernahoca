46fb58193ea93a21de427060f8bf23900bef00f0
TechnoWolves <kaygaz32@gmail.com>
2025-05-22 13:50:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,12 +21,12 @@ function startConfetti(duration = 5000, callback) {
 }
 
 window.addEventListener("load", () => {
-  const video = document.getElementById("birthdayVideo");
+  const ytEmbedUrl = "https://www.youtube.com/embed/nANoHAUAfXM?autoplay=1&mute=1";
+  const videoContainer = document.getElementById("videoContainer");
+  const ytVideo = document.getElementById("ytVideo");
 
   startConfetti(5000, () => {
-    video.style.display = "block";
-    video.play().catch((err) => {
-      console.warn("Video autoplay engellendi:", err);
-    });
+    ytVideo.src = ytEmbedUrl;
+    videoContainer.style.display = "block";
   });
 });
